3a9a9223de61a1dc0ac55731477e8085a58e0453
muhammad bilal qureshi <167212323+bilalqureshi529814@users.noreply.github.com>
2024-07-16 17:42:18 +0500

    Add files via upload

---
+++ q3.js
@@ -0,0 +1,4 @@
+"use strict";
+//  let quote : string = " A person who never made a mistake never tried anything new " 
+//  let author :string= " albert Einstein"
+//  console.log(`${author} once said ${quote}`)
