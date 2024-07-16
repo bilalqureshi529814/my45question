24a706e400e4aa9bc13206421176c2a26175a9e6
muhammad bilal qureshi <167212323+bilalqureshi529814@users.noreply.github.com>
2024-07-16 17:37:10 +0500

    Add files via upload

---
+++ q2.ts
@@ -0,0 +1,5 @@
+
+let personName : string = "bilal qureshi"
+console.log("lowercase :",personName.toLowerCase())
+console.log("uppercase :",personName.toUpperCase())
+console.log("titlecase :",personName.replace(/\b\w/g,c => c .toUpperCase()))
